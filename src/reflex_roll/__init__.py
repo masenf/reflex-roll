283a46f1ea0c6dac467fefd5be666935ef5b7c1a
Masen Furer <m_github@0x26.net>
2024-03-29 06:26:27 -0700

    clean up code and fix version

--- src/reflex_roll/__init__.py
+++ src/reflex_roll/__init__.py
@@ -1,27 +1,34 @@
-"""A more fun loading indicator your App users will love."""
-__version__ = "0.0.1"
+"""A more fun connection down indicator your App users will love."""
+
+import sys
+from base64 import b64encode
+from importlib.resources import files
+from pathlib import Path
+
+ROLL_LOADING_INDICATOR = "rollLoadingIndicator"
+ROLL_FILE_NAME = "ezgif-7-4353d96d35.gif"
 
 try:
-    from base64 import b64encode
-    from importlib.resources import files
-    from pathlib import Path
-
     import reflex.components.core.banner as banner
     from reflex.vars import Var
     from reflex.utils.imports import ImportVar
 
-    class EasterPulser(banner.WifiOffPulse):
+    class RollPulser(banner.WifiOffPulse):
         def _get_imports(self):
             return {
                 **super()._get_imports(),
-                "../utils/roll.js": [ImportVar(tag="rollLoadingIndicator")],
+                "../utils/roll.js": [ImportVar(tag=ROLL_LOADING_INDICATOR)],
             }
 
         def _render(self):
             self.children = []
             tag = super()._render()
             tag.name = "img"
-            tag.add_props(src=Var.create_safe("rollLoadingIndicator", _var_is_local=False, _var_is_string=False))
+            tag.add_props(
+                src=Var.create_safe(
+                    ROLL_LOADING_INDICATOR, _var_is_local=False, _var_is_string=False
+                )
+            )
             tag.remove_props("size")
             return tag
 
@@ -29,11 +36,15 @@ try:
     if web_utils.is_dir():
         roll_js = web_utils / "roll.js"
         if not roll_js.exists():
-            roll = "data:image/gif;base64," + b64encode((files() / "ezgif-7-4353d96d35.gif").read_bytes()).decode()
-            (web_utils / "roll.js").write_text(f"export const rollLoadingIndicator = '{roll}';")
-        banner.WifiOffPulse = EasterPulser
+            roll = (
+                "data:image/gif;base64,"
+                + b64encode((files(__name__) / ROLL_FILE_NAME).read_bytes()).decode()
+            )
+            (web_utils / "roll.js").write_text(
+                f"export const {ROLL_LOADING_INDICATOR} = '{roll}';"
+            )
+        banner.WifiOffPulse = RollPulser
 except ImportError:
-    import sys
     if __name__ in sys.modules:
         try:
             del sys.modules[__name__]
